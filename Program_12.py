fa91b9a8e66b8bac58b023e408d442cc7566f3d5
rafi11225 <60825310+rafi11225@users.noreply.github.com>
2025-04-20 17:10:23 +0200

    Delete Program_12.py

--- Program_12.py
+++
@@ -1,6 +0,0 @@
-
-ludzie = [ ("iohaoiahidaoihdai" , {"name": "Rafał", "surname": "Brzozowski" , "age": 20})]
-
-for ID , slownik in ludzie:
-    for keys in slownik:
-        print(keys, slownik[keys])
