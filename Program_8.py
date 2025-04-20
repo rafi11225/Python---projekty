2aefcbc93f5b8c41ee1c8ddbc581c37b2432accf
rafi11225 <60825310+rafi11225@users.noreply.github.com>
2025-04-20 17:15:48 +0200

    Delete Program_8.py

--- Program_8.py
+++
@@ -1,8 +0,0 @@
-imiona = [ "Rafal" , "Damian" , "Arkadiusz" ]
-imie = str(input("Wprowadz imie do sprawdzenia: "))
-
-if( imie in imiona ):
-    print("Masz dostep")
-    
-elif ( imie not in imiona ):
-    print("Brak dostepu")
