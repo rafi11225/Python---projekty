ced93f1d383b631454817b7932f9b10df617d110
rafi11225 <60825310+rafi11225@users.noreply.github.com>
2025-04-29 23:59:19 +0200

    Update Program_23.py

--- Program_23.py
+++ Program_23.py
@@ -1,8 +1,8 @@
 import requests
-##link = ( "https://www.pornhub.com", "https://www.uzumymw.com", "https://www.GTA-SITE.pl")
+##link = (  "https://www.uzumymw.com", "https://www.GTA-SITE.pl")
 ##for url in link:
   ##  wynik = requests.get(url)
-wynik = requests.get("https://www.pornhub.com") 
+wynik = requests.get(#URL) 
 if(wynik.status_code == 200):
     print("Link działa")
 else:
