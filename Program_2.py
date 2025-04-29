f9dec6b1cbab6891a4c9f0c8d5a1674266526d78
rafi11225 <60825310+rafi11225@users.noreply.github.com>
2025-04-29 23:42:58 +0200

    Delete Program_2.py

--- Program_2.py
+++
@@ -1,8 +0,0 @@
-liczba = 7
-LiczbaProb = 0
-while liczba >= 6:
- liczba = int(input("Podaj  liczbe: "))
- if ( liczba >= 6 ):
-     LiczbaProb += 1
- elif( liczba <= 6 ):
-     print("Uzyto petli " + str(LiczbaProb) + " razy")
